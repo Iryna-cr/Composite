9febf5a5df84590454dc8b34c5261ac05edb5049
Iryna <kytsenkoiryna33@gmail.com>
2025-11-02 22:05:38 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Bouquet
+{
+    private List<string> flowers = new List<string>();
+
+    public void Add(string flower) => flowers.Add(flower);
+    public void Show()
+    {
+        Console.WriteLine("Букет містить:");
+        foreach (var f in flowers) Console.WriteLine(" - " + f);
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        var bouquet = new Bouquet();
+        bouquet.Add("Троянда ");
+        bouquet.Add("Тюльпан ");
+        bouquet.Add("Ромашка ");
+
+        bouquet.Show();
+    }
+}
+
+
+
+
+
+
+
+
